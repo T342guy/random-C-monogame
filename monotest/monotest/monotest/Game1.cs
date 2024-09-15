0a836c6a2bcdb34c9bcd631557b1ffb25a003f99
T342 <147361653+T34256@users.noreply.github.com>
2024-09-15 11:40:59 -0700

    reconfig commit

---
+++ monotest/monotest/monotest/Game1.cs
@@ -0,0 +1,169 @@
+﻿using System.Numerics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Win32;
+
+namespace monotest;
+
+public class Game1 : Game
+{
+    
+    Texture2D melvin; 
+    Microsoft.Xna.Framework.Vector2 melvinposition;
+    float melvinspeed;
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        melvinposition = new Microsoft.Xna.Framework.Vector2(_graphics.PreferredBackBufferWidth / 2,
+                                                            _graphics.PreferredBackBufferHeight / 2);
+         
+            //var melvinsprint = 200f;
+            //var melvinreg = 100f;
+
+//attempt spring mecanic code.
+            //if (melvinspeedsprint = true)
+            //{
+                //melvinspeed = 200f;
+            //}
+            //else if (melvinspeedsprint = false)
+            //{
+                //melvinspeed = 100f;
+            //}
+
+            //if (melvinspeedreg = true)
+            //{
+                //melvinspeed = 100f;
+            //}
+            //else if ( melvinspeedreg = false )
+            //{
+                //melvinspeed = 200f;
+            //}
+
+        melvinspeed = 100f; 
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+
+// declared the render type for melvin 
+        melvin = Content.Load<Texture2D>("melvin");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+        
+        //input/movement logic below
+        float updatedballspeed = melvinspeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        var kstate = Keyboard.GetState();
+
+        if (kstate.IsKeyDown(Keys.W))
+        {
+            melvinposition.Y -= updatedballspeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.S))
+        {
+            melvinposition.Y += updatedballspeed; 
+        }
+
+        if (kstate.IsKeyDown(Keys.A))
+        {
+            melvinposition.X -= updatedballspeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.D))
+        {
+            melvinposition.X += updatedballspeed;
+        }
+        // input logic ends. 
+
+//attempting sprint mecanic
+        //var melvinspeedsprint
+
+        //if (kstate.IsKeyDown(Keys.LeftShift))
+        //{
+            //var melvinspeedsprint = true; 
+        //}
+        //else if (kstate.IsKeyUp(Keys.LeftShift))
+        //{
+            //bool melvinspeedsprint = false;
+        //}
+        
+        //if (kstate.IsKeyDown(Keys.LeftShift))
+        //{
+            //bool melvinspeedreg = false;
+        //}
+        //else if (kstate.IsKeyUp(Keys.LeftShift))
+        //{
+            //bool melvinspeedreg = true;
+        //}
+//end attempt code 
+
+
+        //window borders logic below.
+        if (melvinposition.X > _graphics.PreferredBackBufferWidth - melvin.Width / 2)
+        {
+            melvinposition.X = _graphics.PreferredBackBufferWidth - melvin.Width / 2;
+        }
+        else if (melvinposition.X < melvin.Width / 2)
+        {
+            melvinposition.X = melvin.Width / 2;
+        }
+
+        if (melvinposition.Y > _graphics.PreferredBackBufferHeight - melvin.Height / 2)
+        {
+            melvinposition.Y = _graphics.PreferredBackBufferHeight - melvin.Height / 2;
+        }
+        else if (melvinposition.Y < melvin.Height / 2)
+        {
+            melvinposition.Y = melvin.Height / 2;
+        }
+        //window border logic ends 
+        
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+// TODO: Add your drawing code here
+
+// render for melvin
+    _spriteBatch.Begin();
+    _spriteBatch.Draw(melvin, 
+    melvinposition, 
+    null, 
+    Color.White, 
+    0f, 
+    new Microsoft.Xna.Framework.Vector2(melvin.Width / 2, melvin.Height / 2), 
+    Microsoft.Xna.Framework.Vector2.One, 
+    SpriteEffects.None,
+    0f
+    
+    );
+    
+    
+    _spriteBatch.End();
+        
+        base.Draw(gameTime);
+    }
+}
---
+++ not_a_monogame/not_a_monogame/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new not_a_monogame.Game1();
+game.Run();
